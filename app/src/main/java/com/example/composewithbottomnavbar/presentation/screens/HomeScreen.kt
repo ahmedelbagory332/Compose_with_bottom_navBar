d03c909ea038a3a95cc74a409ffa443552ac5db7
ahmedelbagory332 <ahmedelbagory63@gmail.com>
2025-01-26 15:26:45 +0200

    create Bottom NavBar Screens

---
+++ app/src/main/java/com/example/composewithbottomnavbar/presentation/screens/HomeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.composewithbottomnavbar.presentation.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(onSettingsClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            onSettingsClick()
+        }) {
+            Text(text = "Go to settings screen")
+        }
+    }
+}

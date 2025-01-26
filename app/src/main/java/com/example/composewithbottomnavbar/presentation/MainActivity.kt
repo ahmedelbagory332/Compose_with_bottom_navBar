b1b8519d28d08a67484302e4ad2920fd0f51228f
ahmedelbagory332 <ahmedelbagory63@gmail.com>
2025-01-26 15:58:10 +0200

    add selection color

--- app/src/main/java/com/example/composewithbottomnavbar/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composewithbottomnavbar/presentation/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -31,19 +35,25 @@ class MainActivity : ComponentActivity() {
                         NavigationBar {
                             val navBackStackEntry by navController.currentBackStackEntryAsState()
                             val currentDestination = navBackStackEntry?.destination
+                            var selectedRoute by remember { mutableStateOf(TopLevelRoutes.routes.first().route) }
                             TopLevelRoutes.routes.forEach { topLevelRoute ->
-                                val isSelected =
-                                    currentDestination?.route == topLevelRoute.route.toString()
+                                val isSelected = currentDestination?.route == topLevelRoute.route.toString() || selectedRoute == topLevelRoute.route
+
                                 NavigationBarItem(
                                     icon = {
                                         Icon(
                                             topLevelRoute.icon,
-                                            contentDescription = topLevelRoute.name
+                                            contentDescription = topLevelRoute.name,
+                                            tint = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
                                         )
                                     },
-                                    label = { Text(topLevelRoute.name) },
+                                    label = { Text(
+                                        text = topLevelRoute.name,
+                                        color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
+                                    ) },
                                     selected = isSelected,
                                     onClick = {
+                                        selectedRoute = topLevelRoute.route
                                         navController.navigate(topLevelRoute.route) {
                                             popUpTo(navController.graph.findStartDestination().id) {
                                                 saveState = true
